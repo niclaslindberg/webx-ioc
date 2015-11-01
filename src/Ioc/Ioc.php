5ed38bf80a927bd5b2b354689a78aa2bee6369d0
Niclas Lindberg <niclas.lindberg@tictac.se>
2015-11-01 18:01:43 +0100

    docs

--- src/Ioc/Ioc.php
+++ src/Ioc/Ioc.php
@@ -9,23 +9,24 @@ namespace WebX\Ioc;
 interface Ioc {
 
     /**
-     * Registers this classname or instance with as implementation for all its interfaces with the container.
+     * Registers a className or instance as implementation for all its interfaces with the container.
      * @param string|object $classNameOrObject
      * @return void
      */
     public function register($classNameOrObject);
 
     /**
-     * Returns an instance that implements the interface.
+     * Returns an object (first if multiple) that implements the interface.
      * @param $interfaceName
      * @return object
+     * @throws IocException if no instance that implements the interface can be found.
      */
     public function get($interfaceName);
 
     /**
      * Returns all objects that implement the interface.
      * @param $interfaceName
-     * @return mixed
+     * @return object[]
      */
     public function getAll($interfaceName);
 
