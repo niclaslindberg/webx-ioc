feef2db1fc59cc4c8304254383d573a24db89e2d
Niclas Lindberg <niclas.lindberg@tictac.se>
2015-11-01 19:14:59 +0100

    Docs and removed IOCBootstrap

--- src/Ioc/IocException.php
+++ src/Ioc/IocException.php
@@ -7,7 +7,11 @@
 
 namespace WebX\Ioc;
 
-
+/**
+ * Exception for errors in the IOC container.
+ * Class IocException
+ * @package WebX\Ioc
+ */
 class IocException extends \Exception {
     public function __construct($message, \Exception $cause=null) {
         parent::__construct($message,0,$cause);
