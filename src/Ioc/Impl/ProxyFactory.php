ce7a1c1444590d94a079227d4b0579209639802c
Niclas Lindberg <niclas.lindberg@tictac.se>
2016-01-31 12:34:32 +0100

    Added proxy factory. Fixed type

--- src/Ioc/Impl/ProxyFactory.php
+++ src/Ioc/Impl/ProxyFactory.php
@@ -4,6 +4,7 @@ namespace WebX\Ioc\Impl;
 
 
 use WebX\Ioc\Ioc;
+use WebX\Ioc\IocException;
 
 class ProxyFactory {
 
@@ -23,43 +24,47 @@ class ProxyFactory {
             return $existingProxy;
         } else if ($interfaceClass->isInterface()) {
             if (!class_exists($proxyClassName)) {
-                $classDefParts = [];
-                $classDefParts[] = sprintf('class %s implements \\%s {', $proxyClassName, $interfaceClass->getName());
-                $classDefParts[] = sprintf('private $ioc;');
-                $classDefParts[] = sprintf('private $realObject;');
-                $classDefParts[] = sprintf('public function __construct($ioc) {');
-                $classDefParts[] = sprintf('$this->ioc = $ioc;');
-                $classDefParts[] = sprintf('}');
+                if ($interfaceClass->isInterface()) {
+                    $classDefParts = [];
+                    $classDefParts[] = sprintf('class %s implements \\%s {', $proxyClassName, $interfaceClass->getName());
+                    $classDefParts[] = sprintf('private $ioc;');
+                    $classDefParts[] = sprintf('private $realObject;');
+                    $classDefParts[] = sprintf('public function __construct($ioc) {');
+                    $classDefParts[] = sprintf('$this->ioc = $ioc;');
+                    $classDefParts[] = sprintf('}');
 
-                foreach ($interfaceClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $interfaceMethod) {
-                    $methodParts = [];
-                    $methodParts[] = sprintf("public function %s(", $interfaceMethod->getShortName());
-                    $methodParamDeclarations = [];
-                    $methodParamNames = [];
-                    foreach ($interfaceMethod->getParameters() as $imParam) {
-                        $methodParamName = '$' . $imParam->getName();
-                        $methodParamNames[] = $methodParamName;
-                        if ($imParamClass = $imParam->getClass()) {
-                            if ($imParamClass->isInterface()) {
-                                $methodParamDeclarations[] = sprintf("%s %s", $imParamClass->getName(), $methodParamName);
+                    foreach ($interfaceClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $interfaceMethod) {
+                        $methodParts = [];
+                        $methodParts[] = sprintf("public function %s(", $interfaceMethod->getShortName());
+                        $methodParamDeclarations = [];
+                        $methodParamNames = [];
+                        foreach ($interfaceMethod->getParameters() as $imParam) {
+                            $methodParamName = '$' . $imParam->getName();
+                            $methodParamNames[] = $methodParamName;
+                            if ($imParamClass = $imParam->getClass()) {
+                                if ($imParamClass->isInterface()) {
+                                    $methodParamDeclarations[] = sprintf("%s %s", $imParamClass->getName(), $methodParamName);
+                                } else {
+                                    throw new IocException("Static initilization does not allow classes");
+                                }
                             } else {
-                                throw new IocException("Static initilization does not allow classes");
+                                $methodParamDeclarations[] = $methodParamName;
                             }
-                        } else {
-                            $methodParamDeclarations[] = $methodParamName;
                         }
+                        $methodParts[] = sprintf('%s) {', implode(",", $methodParamDeclarations));
+                        $methodParts[] = sprintf('if (!$this->realObject) {');
+                        $methodParts[] = sprintf('$this->realObject = $this->ioc->get(\'%s\');', $interfaceClass->getName());
+                        $methodParts[] = sprintf('}');
+                        $methodParts[] = sprintf('return $this->realObject->%s(%s);', $interfaceMethod->getName(), implode(",", $methodParamNames));
+                        $methodParts[] = "}";
+                        $classDefParts[] = implode("\n", $methodParts);
                     }
-                    $methodParts[] = sprintf('%s) {', implode(",", $methodParamDeclarations));
-                    $methodParts[] = sprintf('if (!$this->realObject) {');
-                    $methodParts[] = sprintf('$this->realObject = $this->ioc->get(\'%s\');', $interfaceClass->getName());
-                    $methodParts[] = sprintf('}');
-                    $methodParts[] = sprintf('return $this->realObject->%s(%s);', $interfaceMethod->getName(), implode(",", $methodParamNames));
-                    $methodParts[] = "}";
-                    $classDefParts[] = implode("\n", $methodParts);
+                    $classDefParts[] = "}";
+                    $classDef = implode("\n", $classDefParts);
+                    eval($classDef);
+                } else {
+                    throw new IocException("Can not create proxy for '{$interfaceClass}' Not an interface.");
                 }
-                $classDefParts[] = "}";
-                $classDef = implode("\n", $classDefParts);
-                eval($classDef);
             }
             $proxy = new $proxyClassName($this->ioc);
             $this->proxies[$proxyClassName] = $proxy;
